047afcde71b1fe254f1b5a488d1bb20b9ba00381
admin <admin@DESKTOP-ISV2NMC>
2025-07-29 02:38:00 -0700

    first commit

---
+++ test2/src/Day4/Exercise5.java
@@ -0,0 +1,12 @@
+package Day4;
+import java.util.Calendar;
+public class Exercise5 {
+
+	public static void main(String[] args) {
+		 Calendar c = Calendar.getInstance();
+		 System.out.println("Current Date and Time :");
+		 System.out.format("%tB %te, %tY%n", c, c, c);
+		 System.out.format("%tl:%tM %tp%n", c, c, c);
+	}
+
+}
---
+++ test2/src/Day4/Exercise9.java
@@ -0,0 +1,15 @@
+package Day4;
+
+public class Exercise9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "Java Exercises.";
+		 // Convert the above string to a char array.
+		 char[] arr = str.toCharArray();
+		 // Display the contents of the char array.
+		 System.out.println(arr);
+
+	}
+
+}
